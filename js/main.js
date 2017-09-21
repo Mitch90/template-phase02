ed84985e2a24e71ef1473bfe131eca17ccdac0e1
Michele Invernizzi <michele.invernizzi90@gmail.com>
2017-09-21 14:02:24 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // load the interactions only when the document is ready
 $(document).ready(function() {
-    // $('menu-btn').click(function(){
-    //     $(this).addClass("test");
-    // })
+    $('.menu-btn').click(function(){
+        console.log($(this).children('.menu-svg'));
+    })
 });
