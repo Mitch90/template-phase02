72a20b81f5129b0f5580c3f8a5401b44fc70af99
Michele Invernizzi <michele.invernizzi90@gmail.com>
2017-10-03 16:42:54 +0200

    update sidebar

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// load the interactions only when the document is ready
-$(document).ready(function() {
-    $('.menu-btn').click(function(){
-        $(this).children('.menu-svg').toggleClass('scaled');
-        $(this).children('.menu-svg').toggleClass('icon-visible');
-        $('.sidebar').toggleClass('nav-closed nav-open');
-    })
-});
